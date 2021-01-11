59789b0a3050db788c356f2cfddbd9a8b49749eb
Sean Murphy <sdmurphy.1999@gmail.com>
2021-01-10 20:38:22 -0800

    Intial Xcode project added

---
+++ TaskBadger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskBadger
+//
+//  Created by Sean Murphy on 1/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
